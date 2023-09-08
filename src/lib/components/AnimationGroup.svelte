616d3fb936cf32981d1ec97895c031bdf933feca
Luke Parker <lparker@tpx.com>
2023-09-08 14:06:38 -0400

    was able to get the smui components to work. also introduced AnimationGroup

---
+++ src/lib/components/AnimationGroup.svelte
@@ -0,0 +1,30 @@
+<svelte:options customElement={{
+    tag: 'animation-group',
+    shadow: 'none',
+    props: {
+      animationName: { reflect: true, type: 'String', attribute: 'element-index' }
+    }
+}}/>
+
+<script lang="ts">
+	import type { AnimationName } from "$lib";
+	import { onMount } from "svelte";
+
+    const uuid = () => (Math.random() + 1).toString(36).substring(2);
+    console.log(uuid())
+    export let uniqueAnimationGroupId = uuid();
+    export let animationName: AnimationName;
+
+    onMount(() => {
+        const element = document.getElementById(`animg-${uniqueAnimationGroupId}`);
+        if (element) {
+            setTimeout(() => {
+                element.hidden = false;
+            }, 2000)
+        }
+    });
+</script>
+
+<div id="animg-{uniqueAnimationGroupId}" hidden class="magictime {animationName}">
+    <slot></slot>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,37 @@
 <script lang="ts">
 	import { setAnimation, ANIMATIONS } from '$lib';
+	import Card from '@smui/Card';
 	import Button from '@smui/Button';
-	import Card from '@smui/card';
 	import LayoutGrid, { Cell } from '@smui/layout-grid';
+	import AnimationGroup from '$lib/components/AnimationGroup.svelte';
 
 	function randomChoice() {
 		return ANIMATIONS[Math.floor(Math.random() * ANIMATIONS.length)];
 	}
 </script>
 
-<h1 id="title" class="magic-base">Home</h1>
+<div>
+	<h1 id="title" class="magic-base">Home</h1>
 
+	<AnimationGroup animationName="foolishIn">
+		<LayoutGrid>
+			{#each Array(9) as _unused, i}
+			<Cell>
+				<div class="demo-cell">Cell {i + 1}</div>
+			</Cell>
+			{/each}
+		</LayoutGrid>
+	</AnimationGroup>
 
-<Card>
-	This is some card content
-	<Button
-		ripple
-		on:click={() => {
-			setAnimation(randomChoice(), '#title', 1000);
-		}}
-	>
-		Change the animation
-	</Button>
-</Card>
+	<Card>
+		This is some card content
+		<Button
+			ripple
+			on:click={() => {
+				setAnimation(randomChoice(), '#title', 1000);
+			}}
+		>
+			Change the animation
+		</Button>
+	</Card>
+</div>
